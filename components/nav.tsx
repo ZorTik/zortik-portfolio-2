299beeb44d3a3a19dccc9bdcbb2df63ec81169d7
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-06-05 23:58:30 +0200

    Set nav animation only sm and lower

--- components/nav.tsx
+++ components/nav.tsx
@@ -23,7 +23,7 @@ function Navbar({children, links}: NavbarProps) {
             <div className={`w-full border-solid border-b border-b-zinc-900 px-4 md:px-12 md:hidden`}>
                 <button className="w-20 h-20 transition ease-in duration-100 hover:-translate-y-0.5" onClick={handleBurgerClick}><FontAwesomeIcon className="text-white w-6 h-6" icon={faBars} /></button>
             </div>
-            <div className={`w-full border-solid border-b border-b-gray-800 px-4 md:px-12 ${!shown ? "hidden" : "block"} md:block animate-fade-in-top -z-10`}>
+            <div className={`w-full border-solid border-b border-b-gray-800 px-4 md:px-12 ${!shown ? "hidden" : "block"} md:block animate-fade-in-top md:animate-none -z-10`}>
                 <div className="container mx-auto flex flex-col text-center md:text-left md:flex-row py-9">
                     <Link href={"/"} className={`text-white text-2xl hidden md:block`}>ZorTik</Link>
                     <div className="md:ml-auto space-x-10">
