30f6ec2bb6895a39cf9e175eda5bf16bef45c49e
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-06-06 13:51:58 +0200

    Repositories caching

--- data/github.ts
+++ data/github.ts
@@ -4,9 +4,14 @@ import {Repository} from "@/components/home/repositories";
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
 });
+const cache: { [key: string]: Repository[] } = {};
+const cacheTimes: { [key: string]: number } = {};
 
-function fetchRepositories(username: string): Promise<Repository[]> {
-    return octokit.request('GET /users/{username}/repos', {
+async function fetchRepositories(username: string): Promise<Repository[]> {
+    if (cache[username] != null && cacheTimes[username] != null && cacheTimes[username] > Date.now() - 1000 * 60) {
+        return Promise.resolve(cache[username]);
+    }
+    const repositories = await octokit.request('GET /users/{username}/repos', {
         accept: 'application/vnd.github+json',
         username: username,
         type: 'owner',
@@ -22,6 +27,9 @@ function fetchRepositories(username: string): Promise<Repository[]> {
             .filter(repository => repository != null && !repository.private && !repository.fork)
             .sort((a, b) => (b.stargazers_count ?? 0) - (a.stargazers_count ?? 0)))
         .then(data => data as Repository[]);
+    cache[username] = repositories;
+    cacheTimes[username] = Date.now();
+    return repositories;
 }
 
 export {
