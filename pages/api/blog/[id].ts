10810c10a30ec6771634777e4c0195c54d476b97
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-06-21 19:07:22 +0200

    Blog article page SSR

--- pages/api/blog/[id].ts
+++ pages/api/blog/[id].ts
@@ -2,6 +2,7 @@ import {deleteBlogPost, findBlogPost, saveBlogPost} from "@/data/blog";
 import {requireScopesEndpoint} from "@/security/api";
 import prisma from "@/data/prisma";
 import cache from "@/util/cache";
+import {BlogArticle} from "@/components/blog_home/articlecard";
 
 const statStorageCache = cache('stat-storage-cache');
 
@@ -13,20 +14,9 @@ const handler = requireScopesEndpoint(
     const method = req.method!!.toLowerCase();
     const id = parseInt(req.query.id as string);
     if (method === "get") {
-        const article = await findBlogPost(id);
-        if (article.value) {
-            const userId = user?.userId ?? "anonymous";
-            const articleId = article.value.id;
-            // Prevent double counting
-            if (!statStorageCache.getIfPresent({ userId, articleId }) && req.query.statisticsEnabled) {
-                statStorageCache.set({ userId, articleId }, true);
-                await prisma.articleView.create({
-                    data: {
-                        user_id: userId, article_id: articleId, date: new Date(Date.now()),
-                    }
-                });
-            }
-            return res.status(200).json(article.value);
+        const article = await handleGetArticle(id, user?.userId, req.query.statisticsEnabled === "true");
+        if (article) {
+            return res.status(200).json(article);
         } else {
             return res.status(404).json({message: 'Not found'});
         }
@@ -48,5 +38,25 @@ const handler = requireScopesEndpoint(
         }
     }
 }, ['get']);
+
+export async function handleGetArticle(id: number, userId: string|undefined, statisticsEnabled: boolean = false): Promise<BlogArticle|null> {
+    const article = await findBlogPost(id);
+    if (article.value) {
+        const _userId = userId ?? "anonymous";
+        const articleId = article.value.id;
+        // Prevent double counting
+        if (!statStorageCache.getIfPresent({ _userId, articleId }) && statisticsEnabled) {
+            statStorageCache.set({ _userId, articleId }, true);
+            await prisma.articleView.create({
+                data: {
+                    user_id: _userId, article_id: articleId, date: new Date(Date.now()),
+                }
+            });
+        }
+        return article.value;
+    } else {
+        return null;
+    }
+}
 
 export default handler;
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,35 +1,32 @@
 import MainLayout from "@/components/layout/main";
-import {findBlogPost} from "@/data/blog";
-import {GetServerSidePropsContext} from "next";
 import {BlogArticle} from "@/components/blog_home/articlecard";
-import {prepareJsonRender} from "@/util/json";
 import Button from "@/components/button";
-import {MouseEventHandler, useEffect, useState} from "react";
-import {useRouter} from "next/router";
-import {fetchRestrictedApiUrl} from "@/util/api";
+import {MouseEventHandler} from "react";
 import {MdPreview} from "md-editor-rt";
 
 import "md-editor-rt/lib/preview.css"
+import {GetServerSidePropsContext} from "next";
+import {getCookie} from "cookies-next";
+import {USER_NAME_COOKIE_NAME} from "@/data/constants";
+import {handleGetArticle} from "@/pages/api/blog/[id]";
+import {prepareJsonRender} from "@/util/json";
 
-export default function BlogArticlePage() {
-    const [article, setArticle] = useState<BlogArticle|undefined>(undefined);
-    const [fetching, setFetching] = useState<boolean>(true);
-    const {query} = useRouter();
+export async function getServerSideProps({query, req, res}: GetServerSidePropsContext) {
+    const id = Number(query.id);
+    if (isNaN(id)) return { notFound: true };
+
+    const userId = getCookie(USER_NAME_COOKIE_NAME, { req, res }) || undefined;
+    return { props: { article: prepareJsonRender(await handleGetArticle(id, userId as string|undefined, true)) } }
+}
+
+export default function BlogArticlePage({article}: { article?: BlogArticle }) {
     const handleBackClick: MouseEventHandler<HTMLButtonElement> = (e) => {
         e.preventDefault();
         window.open("/blog", "_self");
     }
 
-    useEffect(() => {
-        setFetching(true)
-        fetchRestrictedApiUrl(`/api/blog/${query.id}?statisticsEnabled=true`, { method: 'GET' })
-            .then(res => res.json())
-            .then(res => setArticle(res))
-            .finally(() => setFetching(false));
-    }, [query]);
-
     return (
-        <MainLayout className="flex flex-col items-center" {...{title: article ? article.title : (fetching ? "" : "Article Not Found :(")}}>
+        <MainLayout className="flex flex-col items-center" {...{title: article ? article.title : "Article Not Found :("}}>
             <p className="text-neutral-800">By ZorTik | {new Date(Date.parse(article?.createdAt as any)).toLocaleDateString()}</p>
             {article ? (
                 <article className="w-full lg:w-auto lg:min-w-[800px]">
