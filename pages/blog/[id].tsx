2b7569cbeeb931ee8160a5c11fd4232728549e03
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-08-08 20:27:40 +0200

    Update

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,6 +1,6 @@
 import MainLayout from "@/components/layout/main";
 import {BlogArticle} from "@/components/blog_home/articlecard";
-import Button, {TransparentButton} from "@/components/button";
+import Button from "@/components/button";
 import {MouseEventHandler} from "react";
 import {MdPreview} from "md-editor-rt";
 
@@ -10,9 +10,6 @@ import {getCookie} from "cookies-next";
 import {USER_NAME_COOKIE_NAME} from "@/data/constants";
 import {handleGetArticle} from "@/pages/api/blog/[id]";
 import {prepareJsonRender} from "@/util/json";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import * as regularIcons from "@fortawesome/free-regular-svg-icons";
-import Hr from "@/components/hr";
 
 export async function getServerSideProps({query, req, res}: GetServerSidePropsContext) {
     const id = Number(query.id);
@@ -30,16 +27,12 @@ export default function BlogArticlePage({article}: { article?: BlogArticle }) {
 
     return (
         <MainLayout className="flex flex-col items-center" {...{title: article ? article.title : "Article Not Found :("}}>
-            <p className="text-neutral-800">By ZorTik | {new Date(Date.parse(article?.createdAt as any)).toLocaleDateString()}</p>
+            <p className="text-neutral-800 text-xl">By ZorTik | {new Date(Date.parse(article?.createdAt as any)).toLocaleDateString()}</p>
             {article ? (
                 <article className="w-full lg:w-auto lg:min-w-[800px]">
                     <MdPreview language="en-US" theme="dark" style={{
                         backgroundColor: "transparent"
                     }} modelValue={article.content} />
-                    <div className="w-full flex flex-row p-5 space-x-3">
-                        <TransparentButton><FontAwesomeIcon icon={regularIcons.faThumbsUp} className="w-6 h-6 text-neutral-400" /></TransparentButton>
-                        <p className="text-neutral-400">0x</p>
-                    </div>
                     <div className="w-full text-center mt-28">
                         <Button onClick={handleBackClick}>Back to listing</Button>
                     </div>
