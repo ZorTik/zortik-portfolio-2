357453f5a841dadcab409d9adc64be45939e4389
ZorTik <zortlegit@gmail.com>
2024-05-19 12:51:58 +0200

    Bg dotted on auth

--- components/layout/center.tsx
+++ components/layout/center.tsx
@@ -18,8 +18,8 @@ export default function CenterLayout({title, children, className, backHref}: Cen
             <LoadingIndicator />
             <PopupAlert />
             <div className="flex flex-row w-full min-h-screen bg-black xl:bg-[url('/bg.jpg')] bg-top bg-fixed">
-                <div className="lg:w-1/4 bg-gradient-to-l from-black" />
-                <div className="flex flex-col items-center w-full lg:w-1/2 h-[100vh] bg-black lg:px-40 xl:px-60">
+                <div className="lg:w-1/3 bg-black" />
+                <div className="flex flex-col items-center w-full lg:w-1/3 h-[100vh] bg-black bg-dotted-spacing-4 bg-dotted-neutral-800 lg:px-40 xl:px-60">
                     <div
                         className="w-full sm:w-fit h-fit flex flex-col justify-center pb-16 mt-[5vh]">
                         <h1 className="text-gray-200 text-5xl font-bold mb-12 text-center">{title}</h1>
@@ -30,7 +30,7 @@ export default function CenterLayout({title, children, className, backHref}: Cen
                         <Button className="mt-12 animate-fade-in-top" href={backHref ?? "/"}>Go Back</Button>
                     </div>
                 </div>
-                <div className="lg:w-1/4 bg-gradient-to-r from-black" />
+                <div className="lg:w-1/3 bg-black" />
             </div>
         </>
     )
