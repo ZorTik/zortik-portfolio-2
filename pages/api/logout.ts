02a682bf99b2b6a6536b1f8dd37a20ac0b4e010f
ZorTik <zortlegit@gmail.com>
2025-02-16 01:43:46 +0100

    Clear exp cookieon logout

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {TOKEN_COOKIE_NAME, USER_COOKIE_NAME, USER_NAME_COOKIE_NAME} from "@/data/constants";
+import {TOKEN_COOKIE_NAME, TOKEN_EXP_COOKIE_NAME, USER_COOKIE_NAME, USER_NAME_COOKIE_NAME} from "@/data/constants";
 
 export default async function handler(
     req: NextApiRequest,
@@ -9,6 +9,7 @@ export default async function handler(
         `${USER_COOKIE_NAME}=; HttpOnly; Path=/; Max-Age=0`,
         `${USER_NAME_COOKIE_NAME}=; HttpOnly; Path=/; Max-Age=0`,
         `${TOKEN_COOKIE_NAME}=; HttpOnly; Path=/; Max-Age=0`,
+        `${TOKEN_EXP_COOKIE_NAME}=; HttpOnly; Path=/; Max-Age=0`,
     ]);
     redirect('/?msg=logged_out');
 }
